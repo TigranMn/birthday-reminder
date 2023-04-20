08c6b37ae897653e99b1879e2e548b3d8fef6979
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-04-21 02:51:19 +0400

    changed dark mode detect to class strateg + prefer-color-scheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import Header from '@/components/layout/Header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        if (
+            localStorage.theme === 'dark' ||
+            (!('theme' in localStorage) &&
+                window.matchMedia('(prefers-color-scheme: dark)').matches)
+        ) {
+            document.documentElement.classList.add('dark')
+        } else document.documentElement.classList.remove('dark')
+    }, [])
     return (
         <>
             <Header />
