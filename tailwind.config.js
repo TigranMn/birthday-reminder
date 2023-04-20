841fcb14cfc5815bafba24603e14c7b8bac901d6
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-04-20 19:38:33 +0400

    changes in configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      fontSize: {
-        xxl: '22px',
-        '3xxl': '32px',
-        '5xxl': '52px',
-      },
-      lineHeight: {
-        8.5: '34px',
-        12: '48px',
-        21: '72px',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
+        './src/app/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {
+            fontSize: {
+                xxl: '22px',
+                '3xxl': '32px',
+                '5xxl': '52px'
+            },
+            lineHeight: {
+                8.5: '34px',
+                12: '48px',
+                21: '72px'
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 }
