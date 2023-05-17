2627f66e303b0d82d0065d044986a83c91802789
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-05-18 02:32:08 +0400

    added test cronjob

---
+++ src/pages/api/cron.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export default function handle() {
+  axios.post('api/sign-up', {
+    email: 'cron@mail.ru',
+    password: 'cronjob',
+    fullName: 'Cron Cronovich'
+  })
+}
