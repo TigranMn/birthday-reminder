c3d584aed368fdfead9e5a8367fca956e459b52f
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-05-17 17:23:04 +0400

    change mongo URI

--- .env.local.sh
+++ .env.local.sh
@@ -1,4 +1,5 @@
-export MONGO_URI=mongodb+srv://tgmnatsakanyan:yjdiBP6dIC4qE0Mb@cluster0.eufs3eb.mongodb.net/test
+export MONGO_URI=mongodb+srv://tgmnatsakanyan:OHU2nSc31T2BunyI@cluster0.plylc58.mongodb.net/
+
 export JWT_SECRET=SUPERSECRETKEY
 export NEXTAUTH_SECRET=anotherSuperSecrEtKey
 export NEXTAUTH_URL="http://localhost:3000"
