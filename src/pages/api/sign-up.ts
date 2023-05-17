655b6a47ad1c8c62f060eeb4331bb8ed968c79ac
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-05-17 18:56:12 +0400

    da

--- src/pages/api/sign-up.ts
+++ src/pages/api/sign-up.ts
@@ -1,7 +1,6 @@
 import User from '@/models/userModel'
 import connectMongo from '@/utils/connectMongo'
 import { NextApiRequest, NextApiResponse } from 'next'
-import bcrypt from 'bcrypt'
 // refactor
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
@@ -16,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(409).json({ error: 1, messages: { email: 'Email already in use' } })
       return
     }
-    await User.create({ ...req.body, password: bcrypt.hashSync(req.body.password, 5) })
+    await User.create({ ...req.body, password: req.body.password })
     res.json({ success: true })
   } catch (error: any) {
     res.status(500).json({ error: 1, message: error.message })
