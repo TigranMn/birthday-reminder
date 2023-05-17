0f4301f21ac8eeebb23dbb83d8100480113be3c1
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-05-17 18:30:50 +0400

    comment nextauth env variables for vercel

--- .env.local.sh
+++ .env.local.sh
@@ -1,7 +1,7 @@
 export MONGO_URI=mongodb+srv://tgmnatsakanyan:OHU2nSc31T2BunyI@cluster0.plylc58.mongodb.net/
 
 export JWT_SECRET=SUPERSECRETKEY
-export NEXTAUTH_SECRET=anotherSuperSecrEtKey
-export NEXTAUTH_URL="http://localhost:3000"
+# export NEXTAUTH_SECRET=anotherSuperSecrEtKey
+# export NEXTAUTH_URL="http://localhost:3000"
 
-yarn dev
+yarn dev p
