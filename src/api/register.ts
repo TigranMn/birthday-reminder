78caf3efc8653ff1e805ae031643b6249aa33214
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-05-18 09:54:23 +0400

    fix

--- src/api/register.ts
+++ src/api/register.ts
@@ -1,10 +1,9 @@
 import { TUserState } from '@/redux/slices/userSlice'
 import axios from 'axios'
-import process from 'process'
 
 export const register = async (form: TUserState & { password: string }) => {
   try {
-    await axios.post(`${process.env.NEXTAUTH_URL}/api/sign-up`, form)
+    await axios.post('/api/sign-up', form)
     return { ok: true }
   } catch (e) {
     //@ts-ignore
--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,9 +1,17 @@
-import axios from 'axios'
+import User from '@/models/userModel'
+import connectMongo from '@/utils/connectMongo'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handle() {
-  axios.post('/api/sign-up', {
-    email: 'cron@mail.ru',
-    password: 'cronjob',
-    fullName: 'Cron Cronovich'
-  })
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await connectMongo()
+    await User.create({
+      email: 'cron@mail.ru',
+      password: 'cronjob',
+      fullName: 'Cron Cronovich'
+    })
+    res.json({ success: true })
+  } catch (error: any) {
+    res.status(500).json({ error: 1, message: error.message })
+  }
 }
