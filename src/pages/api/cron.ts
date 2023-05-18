1bcecf635b8fdf36f7f5761e17f37c935818ac71
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-05-18 22:29:20 +0400

    fix cronjob, change crontime

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -49,18 +49,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         toNotify[iterator] = [...toNotify[iterator], notificationInfo]
       }
     }
+
     for (const user in toNotify) {
-      const { username, jubileeName, companyName, age } = toNotify[user]
+      for (const jubilee of toNotify[user]) {
+        const { username, jubileeName, companyName, age } = jubilee
 
-      const mailOptions = {
-        from: `${process.env.MAIL_ORG}" <${process.env.MAIL_USERNAME}>`,
-        to: user,
-        subject: 'Birthday remainder',
-        text: `Hi, ${username}, today is the birthday of your employee ${jubileeName} that works in ${companyName}, he is now ${age} years old`,
-        html: `Hi, <b>${username}</b>, today is the birthday of your employee <b>${jubileeName}</b> that works in <b>${companyName}</b>, he is now <i>${age}</i> years old`
+        const mailOptions = {
+          from: `${process.env.MAIL_ORG}" <${process.env.MAIL_USERNAME}>`,
+          to: user,
+          subject: 'Birthday remainder',
+          text: `Hi, ${username}, today is the birthday of your employee ${jubileeName} that works in ${companyName}, he is now ${age} years old`,
+          html: `Hi, <b>${username}</b>, today is the birthday of your employee <b>${jubileeName}</b> that works in <b>${companyName}</b>, he is now <i>${age}</i> years old`
+        }
+
+        await sendEmail(req, res, mailOptions)
       }
-
-      await sendEmail(req, res, mailOptions)
     }
     res.status(200).send('Emails has been sent')
   } catch (e: any) {
